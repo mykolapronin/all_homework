a1c314e176bd0035300e49d82fe0fffda1f42336
Kolya <you@example.com>
2024-06-06 11:51:00 +0300

    homework reworked_1

--- functions_homework.py
+++ functions_homework.py
@@ -1,12 +1,12 @@
 from pywebio.output import put_html
 
 
-def welcome_user(user_name):
+def greeting_user(user_name):
     welcome_string = put_html(f'<h1>Вітаємо тебе, шановний {user_name}</h1>')
     return str(welcome_string)
 
 
-# nickname = welcome_user('kolya')
+# nickname = greeting_user('kolya')
 # print(nickname)
 
 
