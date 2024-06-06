51a70582053196b17131b75e483893d703844c98
Kolya <you@example.com>
2024-06-07 00:34:14 +0300

    homework reworked_3 XD

--- functions_homework.py
+++ functions_homework.py
@@ -1,9 +1,9 @@
 def greeting_user_with_header_h1(user_name):
-    result = f"<h1>Вітаємо тебе, шановний {username} </h1>"
+    result = f"<h1>Вітаємо тебе, шановний {user_name} </h1>"
     return result
 
 
-def calculating_area_of_rectangle(first_side, second_side)
+def calculating_area_of_rectangle(first_side, second_side):
     area_of_rectangle = first_side * second_side
     return area_of_rectangle
 # s = calculating_area_of_rectangle(6, 4)
